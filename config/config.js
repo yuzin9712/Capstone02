529d2aa0befc054ee5c5d1a3eec3cea68d1a3af7
yuzin9712 <58067265+yuzin9712@users.noreply.github.com>
2020-06-18 18:00:52 +0900

    쪽지함 수정

--- config/config.js
+++ config/config.js
@@ -8,7 +8,6 @@ module.exports = {
         host: 'capstone-project.cojwntxe9hru.ap-northeast-2.rds.amazonaws.com',
         dialect: 'mysql',
         operatorAliases: 'false',
-        logging: false
     },
     production: {
         username: "user",
