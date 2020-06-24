37c0f709f3f038cf80c867b8fbd703dc97479db3
yuzin9712 <58067265+yuzin9712@users.noreply.github.com>
2020-06-25 00:55:06 +0900

    -

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsOptions = {
     origin: true,
     credentials: true
 };
-//확인중,,,,ㅠㅠ
+
 const clientApp = path.join(__dirname + './build');
 
 const pageRouter = require('./routes/page');
