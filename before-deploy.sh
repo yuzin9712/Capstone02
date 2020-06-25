9f449f1f9456a8ef1d7062ef6f85ecfb54575bdc
yuzin9712 <58067265+yuzin9712@users.noreply.github.com>
2020-06-25 19:57:19 +0900

    -

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#cd /home/ubuntu/server #기존에 있는 build파일을 삭제하고 ....
+cd /home/ubuntu/server #기존에 있는 build파일을 삭제하고 ....
 #sudo rm -rf server-latest #기존에 있는 파일을 삭제한다..!
 
-sudo rm -rf server-latest
-sudo rm -rf /home/ubuntu/docker-image/server-latest/*
+sudo rm -rf server-latest/
